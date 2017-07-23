ea1ee719c8f245c2e1198f365a9c3a6b1abdd6e7
Phamminhduy <phamminhduy97@gmail.com>
2017-07-23 14:29:27 +0700

    add code

--- app/src/main/java/com/example/phamm/demoxoaymanhinh/MainActivity.java
+++ app/src/main/java/com/example/phamm/demoxoaymanhinh/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.phamm.demoxoaymanhinh;
 
-import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,16 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-
-        }
-           else if(newConfig.orientation ==Configuration.ORIENTATION_LANDSCAPE){
-
-        }
     }
 }
